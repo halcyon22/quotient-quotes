30d165d23ce2b2e4e6065c2775beb45d12b44d3b
Mike Partridge <mpartridge@assess.com>
2018-05-01 21:09:31 -0500

    adds logic to avoid repetition until all have been used

--- lambda/index.js
+++ lambda/index.js
@@ -11,22 +11,22 @@ exports.handler = (event, context, callback) => {
     if (event.body === null || event.body === undefined) {
         return errorResponse(callback, "Event body not found");
     }
-
+    
     if (isDebugMode()) {
         console.log(event.body);
     }
-
+    
     let body = querystring.parse(event.body);
     if (body.token !== process.env.APP_TOKEN) {
         return errorResponse(callback, "Invalid app token");
     }
-
+    
     respondWithQuote(callback, categories[body.command]);
 };
 
 function respondWithQuote(callback, category) {
 	let params = {
-		"TableName": "quotient-quotes"
+		TableName: process.env.TABLENAME
 	};
 
 	if (category) {
@@ -46,17 +46,59 @@ function respondWithQuote(callback, category) {
     	    if (isDebugMode()) {
     	        console.log(JSON.stringify(data));
     	    }
+    	    
     	    if (data.Items && data.Items.length > 0) {
-                let randomIndex = Math.floor(Math.random() * Math.floor(data.Items.length));
-                let quote = data.Items[randomIndex].quote;
+    	        
+                let leastUsedQuotes = filterByLeastUsed(data.Items);
+                
+                let randomIndex = Math.floor(Math.random() * leastUsedQuotes.length);
+                let randomLeastUsed = leastUsedQuotes[randomIndex];
+                let quote = randomLeastUsed.quote;
                 if (!category) {
-                    quote = `[${data.Items[randomIndex].category}] ${quote}`;
+                    quote = `[${randomLeastUsed.category}] ${quote}`;
                 }
     	        jsonResponse(callback, quote);
+    	        
+    	        incrementUsed(randomLeastUsed);
     	    } else {
     	        jsonResponse(callback, `No quotes found in category ${category}`);
     	    }
     	}
+    });
+}
+
+function filterByLeastUsed(quotes) {
+    let leastUsedCount = quotes.map(function (quote) {
+        return quote.used ? quote.used : 0;
+    }).reduce(function (previous, current) {
+        if (current < previous) {
+            return current;
+        } else {
+            return previous;
+        }
+    });
+    
+    let leastUsedQuotes = quotes.filter(function (quote) {
+       return quote.used == undefined || quote.used == leastUsedCount;
+    });
+    return leastUsedQuotes;
+}
+
+function incrementUsed(quote) {
+    let newValue = quote.used ? quote.used + 1 : 1;
+    
+    let updateparams = {
+        TableName: process.env.TABLENAME,
+        Key: {"uuid": quote.uuid},
+        UpdateExpression: "set #used = :used",
+        ExpressionAttributeNames: {"#used": "used"},
+    	ExpressionAttributeValues: {":used": newValue}
+    };
+
+    documentClient.update(updateparams, function(err, data) {
+        if (err) {
+            console.error("Unable to update: ", JSON.stringify(err, null, 2));
+        }
     });
 }
 
@@ -72,11 +114,11 @@ function jsonResponse(callback, responseText) {
         },
         body: JSON.stringify(responseBody)
     };
-
+    
     if (isDebugMode()) {
         console.log(JSON.stringify(response));
     }
-
+    
     callback(null, response);
     return;
 }
